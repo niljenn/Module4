1ca11d37a4a091e445b090b09f9d81b73df09f5d
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2023-09-24 22:55:25 +0200

    small changes

--- bouncing_balls/Model.java
+++ bouncing_balls/Model.java
@@ -1,6 +1,6 @@
 package bouncing_balls;
 import java.lang.Math;
-import Jama.Matrix;
+
 
 
 /**
@@ -26,37 +26,40 @@ class Model {
 		
 		// Initialize the model with a few balls
 		balls = new Ball[2];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0, g, 2,  0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0, g, 3, 0.3);
+		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0, g, 0.2);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0, g, 0.3);
 	}
 
 	void step(double deltaT) {
-		// separate wall collision handling 
-		for (int i = 0; i < balls.length; i++) {
-			Ball b = balls[i];
-			
-			// detect collision with the border
-			if (b.x < b.radius || b.x > areaWidth - b.radius) {
-				b.vx *= -1; // change direction of ball
-			}
-			if (b.y < b.radius || b.y > areaHeight - b.radius) {
-				b.vy *= -1;
-			}
-			if (b.y < b.radius) {
-				b.y = b.radius; // make sure balls don't go through floor
-			}			
-		}
 
-		// separate ball collision handling
 		for (int i = 0; i < balls.length; i++) {
 			Ball b1 = balls[i];
-			for (int j = i+1; j < balls.length; j++) {
+	
+			// Wall collision handling
+			if (b1.x < b1.radius || b1.x > areaWidth - b1.radius) {
+				b1.vx *= -1; // Change direction of ball
+			}
+			if (b1.y < b1.radius || b1.y > areaHeight - b1.radius) {
+				b1.vy *= -1;
+			}
+			if (b1.x < b1.radius) {
+				b1.x = b1.radius;	// make sure ball don't go through left wall
+			} else if (b1.x > areaWidth - b1.radius) {
+				b1.x = areaWidth - b1.radius; // make sure ball doesn't go through right wall
+			}
+			if (b1.y < b1.radius) {
+				b1.y = b1.radius; // make sure balls don't go through floor
+			} else if (b1.y > areaHeight - b1.radius) {
+				b1.y = areaHeight - b1.radius; // make sure balls don't go through roof
+			}
+	
+			// Ball collision handling
+			for (int j = i + 1; j < balls.length; j++) {
 				Ball b2 = balls[j];
-
-				double dx = b2.x - b1.x;	// distance in x axis
-				double dy = b2.y - b1.y;	// distance in y axis
-				double distance = Math.sqrt(dx * dx + dy * dy); 	// pythagoras theorem
-
+	
+				double dx = b2.x - b1.x; // Distance in x axis
+				double dy = b2.y - b1.y; // Distance in y axis
+				double distance = Math.sqrt(dx * dx + dy * dy); // Pythagoras theorem
 	
 				if (distance < b1.radius + b2.radius) {		// if they have collided:
 					// convert velocities of b1 and b2 from rect to polar
@@ -68,27 +71,17 @@ class Model {
 
 					double new_v1 = beforeR + ((beforeI - beforeR * b1.m)/(b1.m + b2.m));
 					double new_v2 = (beforeI - beforeR * b1.m ) / (b1.m + b2.m);
-
 					
 
                     // swap and reverse velocities
-                    /*double tempP = polarV1[0]; vinkel b1
-                    double tempR = polarV1[1];   radie b1
-					
-                    polarV1[0] = polarV2[0];  vinkel b1 = vinkel b2
-                    polarV1[1] = polarV2[1];  radie b1 = radie b2
-                    polarV2[0] = tempP;       vinkel b2 = vinkel b1
-                    polarV2[1] = tempR;       radie b2 = radie b1
-					*/
-
-					/*double tempP = polarV1[0];
+					double tempP1 = polarV1[0];
 					double tempP2 = polarV2[0];
 
 					polarV1[0] = polarV2[0]; //vinkel b1 = vinkel b2
 					polarV2[0] = polarV1[0];
 
-					polarV2[0] = tempP;  
-					polarV1[0] = tempP2;*/ 
+					polarV2[0] = tempP1;  
+					polarV1[0] = tempP2;
 
 					// convert velocities of b1 and b2 back to rect coordinates
 					double[] rectV1 = polarToRect(polarV1[0], new_v1);
@@ -101,10 +94,9 @@ class Model {
 					b2.vx = rectV2[0];
 					b2.vy = rectV2[1];
 
-					
 				}
 			}
-		
+
 	
 			// compute new velocity
 			b1.vx += deltaT * b1.ax;
@@ -160,15 +152,15 @@ class Model {
 	 */
 	class Ball {
 		
-		Ball(double x, double y, double vx, double vy, double ax, double gy, double m, double r) {
+		Ball(double x, double y, double vx, double vy, double ax, double gy, double r) {
 			this.x = x; // x(t) (old) position
 			this.y = y; // y(t) (old) position 
 			this.vx = vx; // x'(t) velocity in x
 			this.vy = vy; // y'(t) velocity in y
 			this.ax = ax; // x''(t) acceleration in x
 			this.gy = gy; // y''(t) acceleration in y
-			this.m = m;
-			this.radius = r;
+			this.radius = r; // radius
+			this.m = Math.PI * Math.pow(r, 2); // mass
 		}
 
 		/**
